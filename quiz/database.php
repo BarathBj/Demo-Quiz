836bc9976fbf0dc86ee9b6a69eca135b8836a0de
BarathBj <94238296+BarathBj@users.noreply.github.com>
2023-07-25 18:25:11 +0530

    Update database.php

--- quiz/database.php
+++ quiz/database.php
@@ -1,3 +1,3 @@
 <?php
-$con= new mysqli('localhost:0443','root','','quiz_db')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('localhost:80443','root','','quiz_db')or die("Could not connect to mysql".mysqli_error($con));
 ?>
